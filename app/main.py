a7fc42d2e164b87c0a0d44ed232799ea7afe38c6
sandeepkumar9760 <sandeeproofingsarvice@gmail.com>
2026-09-21 12:00:40 +0530

    testing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
+from http.client import HTTPException
+
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+import logging
 
 from fastapi import FastAPI
 from app.schemas import CustomerData ,   PredictionResponse
 
+logging.basicConfig(
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 mlflow.set_tracking_uri(
     "sqlite:///C:/Users/sande/OneDrive/Desktop/Telco-Customer-Churn-model/notebooks/mlflow.db"
@@ -43,6 +51,8 @@ def home():
 @app.get("/health")
 def health():
 
+    logger.info("Health check requested")
+
     return {
         "status": "healthy",
         "model_loaded": model is not None
@@ -55,37 +65,54 @@ def health():
 )
 def predict(customer: CustomerData):
 
-    customer_data = {
-        "Gender": customer.Gender,
-        "Senior Citizen": customer.Senior_Citizen,
-        "Partner": customer.Partner,
-        "Dependents": customer.Dependents,
-        "Tenure Months": customer.Tenure_Months,
-        "Phone Service": customer.Phone_Service,
-        "Multiple Lines": customer.Multiple_Lines,
-        "Internet Service": customer.Internet_Service,
-        "Online Security": customer.Online_Security,
-        "Online Backup": customer.Online_Backup,
-        "Device Protection": customer.Device_Protection,
-        "Tech Support": customer.Tech_Support,
-        "Streaming TV": customer.Streaming_TV,
-        "Streaming Movies": customer.Streaming_Movies,
-        "Contract": customer.Contract,
-        "Paperless Billing": customer.Paperless_Billing,
-        "Payment Method": customer.Payment_Method,
-        "Monthly Charges": customer.Monthly_Charges,
-        "Total Charges": customer.Total_Charges
-    }
+    try:
 
-    input_df = pd.DataFrame([customer_data])
+        customer_data = {
+            "Gender": customer.Gender,
+            "Senior Citizen": customer.Senior_Citizen,
+            "Partner": customer.Partner,
+            "Dependents": customer.Dependents,
+            "Tenure Months": customer.Tenure_Months,
+            "Phone Service": customer.Phone_Service,
+            "Multiple Lines": customer.Multiple_Lines,
+            "Internet Service": customer.Internet_Service,
+            "Online Security": customer.Online_Security,
+            "Online Backup": customer.Online_Backup,
+            "Device Protection": customer.Device_Protection,
+            "Tech Support": customer.Tech_Support,
+            "Streaming TV": customer.Streaming_TV,
+            "Streaming Movies": customer.Streaming_Movies,
+            "Contract": customer.Contract,
+            "Paperless Billing": customer.Paperless_Billing,
+            "Payment Method": customer.Payment_Method,
+            "Monthly Charges": customer.Monthly_Charges,
+            "Total Charges": customer.Total_Charges
+        }
 
-    probability = model.predict_proba(input_df)[0, 1]
+        input_df = pd.DataFrame([customer_data])
 
-    prediction = int(probability >= THRESHOLD)
+        probability = model.predict_proba(input_df)[0, 1]
 
-    return {
-        "churn_probability": round(float(probability), 4),
-        "churn_prediction": prediction,
-        "churn_label": "Yes" if prediction == 1 else "No",
-        "threshold": THRESHOLD
-    }
+        prediction = int(probability >= THRESHOLD)
+
+        logger.info(
+            "Churn prediction generated successfully"
+        )
+
+        return {
+            "churn_probability": round(float(probability), 4),
+            "churn_prediction": prediction,
+            "churn_label": "Yes" if prediction == 1 else "No",
+            "threshold": THRESHOLD
+        }
+
+    except Exception as e:
+
+        logger.exception(
+            "Error while generating churn prediction"
+        )
+
+        raise HTTPException(
+            status_code=500,
+            detail="Unable to generate churn prediction"
+        )
